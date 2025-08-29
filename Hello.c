dd2c54880f7e3d5c4b2a2fadfced869b64294e4a
Array Blog <vishalupadhyay.rsm@gmail.com>
2025-08-29 19:04:00 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,12 @@
+// This tells teh compiler to include Standsrd Input/output  Liberary
+#include <stdio.h>
+
+//  Where teh excecution start
+int main()
+{
+    // sort form of print
+    printf("World Hello \n");
+
+    // This tells the opertaing system the progrma is ended
+    return 0;
+}
